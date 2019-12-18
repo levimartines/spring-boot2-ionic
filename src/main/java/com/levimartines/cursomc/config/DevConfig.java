5145a738919180a4e16f5c1448737d05eb36e3ec
levimartines <levi.mferreira@gmail.com>
2019-12-18 00:50:40 -0300

    Criando Profile Dev

---
+++ src/main/java/com/levimartines/cursomc/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.levimartines.cursomc.config;
+
+import com.levimartines.cursomc.service.DbService;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!strategy.equals("create")) {
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
