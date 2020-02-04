aa8cb46ab35e28ed1a94abb0c42bea628a47ce2a
levimartines <levi.mferreira@gmail.com>
2020-02-03 21:21:57 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/levimartines/cursomc/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.levimartines.cursomc.config;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC_MATCHERS = {
+        "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+        "/produtos/**",
+        "/categorias/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            httpSecurity.headers().frameOptions().disable();
+        }
+
+        httpSecurity.cors().and().csrf().disable();
+        httpSecurity.authorizeRequests()
+            .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+            .antMatchers(PUBLIC_MATCHERS).permitAll()
+            .anyRequest().authenticated();
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
