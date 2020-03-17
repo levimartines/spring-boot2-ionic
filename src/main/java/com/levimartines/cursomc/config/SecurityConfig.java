d1171f656409b227b0b54ccb81b1b541d37c58b5
levimartines <levi.mferreira@gmail.com>
2020-03-16 22:54:28 -0300

    Documentation with Swagger

--- src/main/java/com/levimartines/cursomc/config/SecurityConfig.java
+++ src/main/java/com/levimartines/cursomc/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -86,5 +87,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+            "/configuration/**",
+            "/swagger-ui.html", "/webjars/**");
     }
 }
--- src/main/java/com/levimartines/cursomc/controller/CategoriaController.java
+++ src/main/java/com/levimartines/cursomc/controller/CategoriaController.java
@@ -3,6 +3,9 @@ package com.levimartines.cursomc.controller;
 import com.levimartines.cursomc.bean.CategoriaBean;
 import com.levimartines.cursomc.model.Categoria;
 import com.levimartines.cursomc.service.CategoriaService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,30 +37,36 @@ public class CategoriaController {
     }
 
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Busca por id")
     public ResponseEntity<Categoria> findById(@PathVariable Long id) {
         return ResponseEntity.ok(categoriaService.findById(id));
     }
 
-    @RequestMapping(value="/page", method= RequestMethod.GET)
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    @ApiOperation(value = "Retorna todas categorias com paginação")
     public ResponseEntity<Page<CategoriaBean>> findPage(
-        @RequestParam(value="page", defaultValue="0") Integer page,
-        @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
-        @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
-        @RequestParam(value="direction", defaultValue="ASC") String direction) {
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+        @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+        @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
         Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
         Page<CategoriaBean> listDto = list.map(CategoriaBean::new);
         return ResponseEntity.ok().body(listDto);
     }
 
     @GetMapping
-    public ResponseEntity<?> findAll(){
+    @ApiOperation(value = "Retorna todas categorias")
+    public ResponseEntity<?> findAll() {
         List<Categoria> list = categoriaService.findAll();
-        return ResponseEntity.ok(list.stream().map(CategoriaBean::new).collect(Collectors.toList()));
+        return ResponseEntity
+            .ok(list.stream().map(CategoriaBean::new).collect(Collectors.toList()));
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaBean obj, HttpServletRequest request) {
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @ApiOperation(value = "Insere categoria")
+    public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaBean obj,
+        HttpServletRequest request) {
         categoriaService.save(categoriaService.fromBean(obj));
         URI uri = ServletUriComponentsBuilder.fromRequest(request)
             .path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -66,6 +75,7 @@ public class CategoriaController {
 
     @PutMapping(value = "/{id}")
     @PreAuthorize("hasAnyRole('ADMIN')")
+    @ApiOperation(value = "Atualiza categoria")
     public ResponseEntity update(@Valid @RequestBody CategoriaBean obj, @PathVariable Long id) {
         obj.setId(id);
         categoriaService.update(categoriaService.fromBean(obj));
@@ -74,7 +84,11 @@ public class CategoriaController {
 
     @DeleteMapping(value = "/{id}")
     @PreAuthorize("hasAnyRole('ADMIN')")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    @ApiOperation(value = "Remove categoria")
+    @ApiResponses(value = {
+        @ApiResponse(code = 400, message = "Não é possível excluir uma categoria que possui produtos"),
+        @ApiResponse(code = 404, message = "Código inexistente")})
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
     }
