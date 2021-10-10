67ac75461c4a6c8ccf8f26b76ffd4a526fe4180d
Levi Martines <levi.mferreira@gmail.com>
2021-10-10 12:32:30 -0300

    Generating test report with JaCoCo

---
+++ src/test/java/com/levimartines/cursomc/service/DbServiceTest.java
@@ -0,0 +1,19 @@
+package com.levimartines.cursomc.service;
+
+import java.text.ParseException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceTest {
+
+	@Autowired
+	DbService dbService;
+
+	@Test
+	void testInstantiateTestDb() throws ParseException {
+		dbService.instantiateTestDatabase();
+	}
+}
